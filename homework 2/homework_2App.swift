0d1380114196333645268e13e70fefcb3e76a7c7
chungweichung <bio891128@gmail.com>
2022-10-17 11:21:55 +0800

    Initial Commit

---
+++ homework 2/homework_2App.swift
@@ -0,0 +1,17 @@
+//
+//  homework_2App.swift
+//  homework 2
+//
+//  Created by Weichung Chung on 2022/10/17.
+//
+
+import SwiftUI
+
+@main
+struct homework_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
